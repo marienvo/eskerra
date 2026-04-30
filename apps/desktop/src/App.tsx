48d83774da278f4910c84518f31f50dd3028cc69
Marien van Overbeek <git@marienvanoverbeek.nl>
2026-04-30 18:14:56 +0200

    Moved one local App hook

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -71,7 +71,6 @@ import {
 import {
   DEFAULT_LAYOUTS,
   loadStoredLayouts,
-  saveStoredLayouts,
   type StoredLayouts,
 } from './lib/layoutStore';
 import {hydrateEmojiUsageFromStore} from './lib/emojiUsageStore';
@@ -89,6 +88,7 @@ import {
 } from './lib/resolveAppStatusBarCenter';
 import {createTauriVaultFilesystem} from './lib/tauriVault';
 import {writeVaultSettings} from './lib/vaultBootstrap';
+import {useAppLayoutWidthPersisters} from './shell/useAppLayoutWidthPersisters';
 import {useAppMainWindowKeyboardEffects} from './shell/useAppMainWindowKeyboardEffects';
 import {useAppNotificationSession} from './shell/useAppNotificationSession';
 import {useAppRootClassName} from './shell/useAppRootClassName';
@@ -730,62 +730,6 @@ function useAppTauriCloseAndFocusSave(
       unlistenFocus?.();
     };
   }, [desktopPlaybackRef, flushInboxSave]);
-}
-
-function useAppLayoutWidthPersisters(
-  setLayouts: Dispatch<SetStateAction<StoredLayouts>>,
-) {
-  const persistMainLeftWidthPx = useCallback((leftWidthPx: number) => {
-    setLayouts(prev => {
-      const next = {
-        ...prev,
-        inbox: {leftWidthPx},
-        podcastsMain: {leftWidthPx},
-      };
-      void saveStoredLayouts(next);
-      return next;
-    });
-  }, [setLayouts]);
-
-  const persistVaultEpisodesStackTopHeightPx = useCallback(
-    (topHeightPx: number) => {
-      setLayouts(prev => {
-        const next = {...prev, vaultEpisodesStack: {topHeightPx}};
-        void saveStoredLayouts(next);
-        return next;
-      });
-    },
-    [setLayouts],
-  );
-
-  const persistNotificationsInboxStackTopHeightPx = useCallback(
-    (topHeightPx: number) => {
-      setLayouts(prev => {
-        const next = {...prev, notificationsInboxStack: {topHeightPx}};
-        void saveStoredLayouts(next);
-        return next;
-      });
-    },
-    [setLayouts],
-  );
-
-  const persistNotificationsWidthPx = useCallback(
-    (widthPx: number) => {
-      setLayouts(prev => {
-        const next = {...prev, notifications: {widthPx}};
-        void saveStoredLayouts(next);
-        return next;
-      });
-    },
-    [setLayouts],
-  );
-
-  return {
-    persistMainLeftWidthPx,
-    persistVaultEpisodesStackTopHeightPx,
-    persistNotificationsInboxStackTopHeightPx,
-    persistNotificationsWidthPx,
-  };
 }
 
 type AppThemeShellProps = {
